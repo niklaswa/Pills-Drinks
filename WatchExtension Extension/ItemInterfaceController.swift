d1b66912b9d69e61a15d3ef9ccfff25bc77583bf
Niklas <git@niklas.top>
2018-04-23 16:07:07 +0200

    testing watch table

--- WatchExtension Extension/ItemInterfaceController.swift
+++ WatchExtension Extension/ItemInterfaceController.swift
@@ -1,44 +1,67 @@
 //
-//  InterfaceController.swift
+//  ItemInterfaceController.swift
 //  WatchExtension Extension
 //
-//  Created by Niklas on 16.04.18.
+//  Created by Niklas on 23.04.18.
 //  Copyright © 2018 Niklas. All rights reserved.
 //
 
+import UIKit
 import WatchKit
 import Foundation
 import WatchConnectivity
 
-class InterfaceController: WKInterfaceController, WCSessionDelegate {
+class ItemInterfaceController: WKInterfaceController, WCSessionDelegate {
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         // Configure interface objects here.
     }
+
+    @IBOutlet var itemTable: WKInterfaceTable!
+    
+    var data = ["Fanta","Cola","Sprite"]
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
         
-        
         if WCSession.isSupported() {
             let session = WCSession.default
             session.delegate = self
             session.activate()
+            
+            session.sendMessage(
+                ["request" : "items"],
+                replyHandler: { (response) in
+                    print(response)
+                    //data.append()
+                },
+                errorHandler: { (error) in
+                    print("Error sending message: %@", error)
+                }
+            )
+        }
+        
+        
+    }
+    
+    
+    func tableRefresh(){
+        itemTable.setNumberOfRows(data.count, withRowType: "row")
+        for item in data {
+            let row = itemTable.insertRows(at: data.index(of: item)!, withRowType: nil)
+            row.myLabel.setText(item)
         }
     }
     
+
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
 
-    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
-        WKInterfaceDevice().play(.success)
-    }
-    
     func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
         
     }
---
+++ WatchExtension Extension/ItemRowController.swift
@@ -0,0 +1,14 @@
+//
+//  ItemRowController.swift
+//  WatchExtension Extension
+//
+//  Created by Niklas on 23.04.18.
+//  Copyright © 2018 Niklas. All rights reserved.
+//
+
+import Foundation
+import WatchKit
+
+class ItemRowController: NSObject {
+    
+}
